2283c110d8718066afb1578407ea9271c5272f6b
tlotli <tlotlimosalo@gmail.com>
2016-04-26 08:33:27 +0200

    Removed Yerrrrrrrrrrrrr

--- js/index.js
+++ js/index.js
@@ -16,7 +16,21 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- 
+  document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal.init("71dd5fd3-a846-4d43-a44f-e1cc55aadb80",
+                                 {googleProjectNumber: "25060886627"},
+                                 notificationOpenedCallback);
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
+}, false);
 
  
 var app = {
@@ -38,22 +52,6 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 		
- document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal.init("71dd5fd3-a846-4d43-a44f-e1cc55aadb80",
-                                 {googleProjectNumber: "25060886627"},
-                                 notificationOpenedCallback);
-  
-  // Show an alert box if a notification comes in when the user is in your app.
-  window.plugins.OneSignal.enableInAppAlertNotification(true);
-}, false);
- 		
 		
     },
     // Update DOM on a Received Event
